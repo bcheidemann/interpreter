817ec7f54c50f19776483291b90b59a24622e882
Ben Heidemann <ben@heidemann.co.uk>
2022-06-03 19:02:30 +0100

    interactive expression evaluator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
+use std::io;
+use std::io::*;
+
 mod lib;
 
 fn main() {
-    let mut scanner = lib::scanner::Scanner::from_source("123 + 5");
-    let mut parser = lib::parser::Parser::new(scanner.scan_tokens().unwrap());
-    let expression = parser.parse();
-    let result = lib::interpreter::evaluate_expression(expression);
-    println!("{result:?}");
+    let mut stdout = io::stdout().lock();
+    let mut stdin = io::stdin().lock();
+
+    loop {
+        let mut input = String::new();
+
+        stdout.write(">> ".as_bytes()).unwrap();
+        stdout.flush().unwrap();
+        stdin
+            .read_line(&mut input)
+            .expect("error: unable to read user input");
+
+        if input == "exit\n" {
+            break;
+        }
+
+        let mut scanner = lib::scanner::Scanner::from_source(&input);
+        let mut parser =
+            lib::parser::Parser::new(scanner.scan_tokens().expect("Failed at scanner"));
+        let expression = parser.parse();
+        let result = lib::interpreter::evaluate_expression(expression);
+
+        println!("{result:?}");
+    }
 }
